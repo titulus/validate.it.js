3ebc507760a62a360b7e3d31b717118d568d1519
Kuntsevich Andrei <kuntsevich@siberian.pro>
2018-02-15 10:00:13 +0300

    hasLetters

--- asserts/hasLettersLatin.js
+++ asserts/hasLettersLatin.js
@@ -1,12 +1,7 @@
 const has = require('./has.js');
 
 module.exports = base => {
-  const ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz';
-  const ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const letters = [
-    ...ascii_lowercase.split(''),
-    ...ascii_uppercase.split(''),
-  ];
+  const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   const error = has(base, ...letters);
   if (error)
     return {
