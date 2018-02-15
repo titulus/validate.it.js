499c031070f2cc59eb106b871572be4912d9ed30
Kuntsevich Andrei <kuntsevich@siberian.pro>
2018-02-15 10:29:18 +0300

    refactor hasNo like .has

--- asserts/hasNo.js
+++ asserts/hasNo.js
@@ -1,17 +1,29 @@
 const match = require('./match.js');
 
-module.exports = (base, pattern) => {
-  if (!pattern)
-    throw new Error('pattern must be specified');
+module.exports = (base, ...subStrings) => {
+  if (subStrings.length === 0)
+    throw new Error('at least 1 substring must be specified');
 
-  const regexp = new RegExp('' + pattern);
-  const found = regexp.test('' + base);
+  const string = '' + base;
+
+  let found = false;
+  const founded = [];
+  for (const subString of subStrings) {
+    const position = string.indexOf(subString);
+    if (position === -1)
+      continue;
+
+    found = true;
+    founded.push({subString, position});
+  };
+
   if (found)
     return {
-      string: base,
-      pattern,
-      found: true,
-      message: `"${pattern}" found in "${base}"`,
+      string,
+      subStrings,
+      founded,
+      found,
+      message: `${JSON.stringify(founded.map(({substring}) => substring))} found in "${string}"`,
     };
 
   return;
--- test/asserts/hasNo.test.js
+++ test/asserts/hasNo.test.js
@@ -6,16 +6,15 @@ describe('asserts/hasNo', () => {
   describe('ok', () => {
     it('on char', () => expect(hasNo('abc123', 'e')).to.not.be.ok);
     it('on digit', () => expect(hasNo('abc123', 4)).to.not.be.ok);
-    it('on digit as char', () => expect(hasNo('abc123', '4')).to.not.be.ok);
-    it('on regexp pattern', () => expect(hasNo('abc123', '\\s')).to.not.be.ok);
-    it('on regexp pattern', () => expect(hasNo('abc123', '[А-я]')).to.not.be.ok);
+    it('on substring', () => expect(hasNo('abc123', 'd2')).to.not.be.ok);
+    it('on set (all not present)', () => expect(hasNo('abc123', 'e', 4, 'd2')).to.not.be.ok);
   });
 
   describe('!ok', () => {
     it('on char', () => expect(hasNo('abc123', 'b')).to.be.ok);
     it('on digit', () => expect(hasNo('abc123', 2)).to.be.ok);
-    it('on digit as char', () => expect(hasNo('abc123', '2')).to.be.ok);
-    it('on regexp pattern', () => expect(hasNo('abc123', '\\d')).to.be.ok);
-    it('on regexp pattern', () => expect(hasNo('abc123', '[A-z]')).to.be.ok);
+    it('on substring', () => expect(hasNo('abc123', 'c1')).to.be.ok);
+    it('on set (all present)', () => expect(hasNo('abc123', 'b', 2, 'c1')).to.be.ok);
+    it('on set (one of present)', () => expect(hasNo('abc123', 'b', 4)).to.be.ok);
   });
 });
