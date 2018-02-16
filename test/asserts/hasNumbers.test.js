d9df25e941722542841d93fbe21149bbf6e225f8
Kuntsevich Andrei <kuntsevich@siberian.pro>
2018-02-16 05:43:01 +0300

    test returned for .hasNumbers

--- test/asserts/hasNumbers.test.js
+++ test/asserts/hasNumbers.test.js
@@ -14,4 +14,15 @@ describe('asserts/hasNumbers', () => {
     it('on "abc"', () => expect(hasNumbers("abc")).to.be.ok);
     it('on NaN', () => expect(hasNumbers(NaN)).to.be.ok);
   });
+
+  describe('returned object', () => {
+    it('.string', () => expect(hasNumbers('abc').string).to.equal('abc'));
+    it('.subStrings', () => {
+      const result = hasNumbers('abc');
+      expect(result).to.have.ownPropertyDescriptor('subStrings');
+      expect(JSON.stringify(result.subStrings)).to.equal('[1,2,3,4,5,6,7,8,9,0]');
+    });
+    it('.found === false', () => expect(hasNumbers('abc').found).to.be.false);
+    it('.message', () => expect(hasNumbers('abc').message).to.be.ok);
+  });
 });
