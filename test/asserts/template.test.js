e45b69f29df6baa4b82c77686fade038b12b1f65
Kuntsevich Andrei <kuntsevich@siberian.pro>
2018-02-16 05:47:40 +0300

    test returned for ._template

--- test/asserts/template.test.js
+++ test/asserts/template.test.js
@@ -13,4 +13,14 @@ describe('asserts/template', () => {
     it('on ("abc", "def")', () => expect(template("abc", "def")).to.be.ok);
     it('on ("abc", 1, 2, 3)', () => expect(template("abc", 1, 2, 3)).to.be.ok);
   });
+
+  describe('returned', () => {
+    it('.base', () => expect(template("abc", 1, 2, 3).base).to.equal('abc'));
+    it('.args', () => {
+      const result = template("abc", 1, 2, 3);
+      expect(result).to.have.ownPropertyDescriptor('args');
+      expect(JSON.stringify(result.args)).to.equal('[1,2,3]');
+    });
+    it('.message', () => expect(template("abc", 1, 2, 3).message).to.equal('this is template assert'));
+  });
 });
