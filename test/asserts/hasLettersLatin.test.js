38d89c85edf32478dc6f52e2b098af055996ffa6
Kuntsevich Andrei <kuntsevich@siberian.pro>
2018-02-16 05:41:47 +0300

    test returned for .hasLettersLatin

--- test/asserts/hasLettersLatin.test.js
+++ test/asserts/hasLettersLatin.test.js
@@ -15,4 +15,15 @@ describe('asserts/hasLettersLatin', () => {
     it('on special chars', () => expect(hasLettersLatin('!@#$%^&*(')).to.be.ok);
     it('on no-latin', () => expect(hasLettersLatin('абв')).to.be.ok);
   });
+
+  describe('returned object', () => {
+    it('.string', () => expect(hasLettersLatin('абв123').string).to.equal('абв123'));
+    it('.subStrings', () => {
+      const result = hasLettersLatin('абв123');
+      expect(result).to.have.ownPropertyDescriptor('subStrings');
+      expect(JSON.stringify(result.subStrings)).to.equal('["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]');
+    });
+    it('.found === false', () => expect(hasLettersLatin('абв123').found).to.be.false);
+    it('.message', () => expect(hasLettersLatin('абв123').message).to.be.ok);
+  });
 });
